af976dd91b179b519286d6b613d4a00c18215623
bhargavgurugubelli <gurugubellibhargav@gmail.com>
2025-07-23 08:56:15 +0530

    fix: removed invalid GA ID causing blank screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-// Initialize react-ga with your Google Analytics tracking ID
-ReactGA.initialize('YOUR-GOOGLE-ANALYTICS-MEASUREMENT-ID');
-ReactGA.pageview(window.location.pathname + window.location.search);
+// // Initialize react-ga with your Google Analytics tracking ID
+// ReactGA.initialize('YOUR-GOOGLE-ANALYTICS-MEASUREMENT-ID');
+// ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
